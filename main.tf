9996fb948eabbf6069078da36c380046f90a93a7
mmr116 <38760695+mmr116@users.noreply.github.com>
2024-05-11 14:44:41 -0400

    Initial commit

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  # Define required MongoDB provider and versions
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
@@ -6,7 +7,7 @@ terraform {
     }
   }
 }
-
+# Define the Azure provider block and authentication configuration using service principal
 provider "azurerm" {
   features {}
   
@@ -16,6 +17,7 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
+# Retrieve MongoDB Atlas API secrets from Azure Key Vault
 data "azurerm_key_vault_secret" "mongodb_atlas_api_private_key" {
   name         = "mongodb-atlas-api-private-key"
   key_vault_id = var.key_vault_id
@@ -31,16 +33,19 @@ data "azurerm_key_vault_secret" "mongodb_atlas_organization_id" {
   key_vault_id = var.key_vault_id
 }
 
+# Define the MongoDB Atlas provider block
 provider "mongodbatlas" {
   public_key  = data.azurerm_key_vault_secret.mongodb_atlas_api_public_key.value
   private_key = data.azurerm_key_vault_secret.mongodb_atlas_api_private_key.value
 }
 
+# Define the MongoDB Atlas project resource
 resource "mongodbatlas_project" "example" {
   name        = var.project_name
   org_id      = data.azurerm_key_vault_secret.mongodb_atlas_organization_id.value
 }
 
+# Define the MongoDB Atlas cluster resource
 resource "mongodbatlas_cluster" "example" {
   project_id                  = mongodbatlas_project.example.id
   name                        = var.cluster_name
@@ -48,16 +53,18 @@ resource "mongodbatlas_cluster" "example" {
   mongo_db_major_version      = var.mongo_db_major_version
   provider_name               = var.provider_name
   provider_instance_size_name = var.instance_size_name
-  
+
+  # Specify auto-scaling configuration
   auto_scaling_compute_enabled                    = var.auto_scaling_enabled
   auto_scaling_compute_scale_down_enabled         = var.auto_scale_down_enabled
   provider_auto_scaling_compute_max_instance_size = var.max_instance_size
   provider_auto_scaling_compute_min_instance_size = var.min_instance_size
-  
+
+  # Specify cloud backup and continous cloud backup configuration
   cloud_backup = var.cloud_backup
   pit_enabled  = var.pitr_backup_enabled
   
- 
+  # Specify replication configuration in a multi-region cluster
   replication_specs {
     num_shards = 1
     regions_config {
@@ -79,7 +86,8 @@ resource "mongodbatlas_cluster" "example" {
       read_only_nodes = 0
     }
   }
-  
+
+  # Specify tags for the cluster
   tags {
     key   = var.tag_env
     value = var.tag_env_value
